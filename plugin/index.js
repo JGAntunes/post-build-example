647caefd2b2f6e45f25219501c3f7cffc3f5d684
JGAntunes <joao@netlify.com>
2022-07-21 20:00:15 +0100

    add plugin logic

---
+++ plugin/index.js
@@ -0,0 +1,14 @@
+import { resolve } from "path";
+import { appendFile } from "fs/promises";
+
+import { setTimeout } from "timers/promises";
+
+export const onPostBuild = async function () {
+  console.log("start plugin");
+  await setTimeout(10 * 1000);
+  appendFile(
+    resolve("./dist/index.html"),
+    `<div>Plugin run ${Date.now()}</div>`
+  );
+  console.log("done plugin");
+};
