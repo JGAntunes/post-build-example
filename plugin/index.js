53025636dc66da5e648e089f177c8f49be6686f8
JGAntunes <joao@netlify.com>
2022-07-21 20:05:37 +0100

    test plugin logic

--- plugin/index.js
+++ plugin/index.js
@@ -1,14 +1,14 @@
 import { resolve } from "path";
 import { appendFile } from "fs/promises";
 
-import { setTimeout } from "timers/promises";
+// import { setTimeout } from "timers/promises";
 
 export const onPostBuild = async function () {
   console.log("start plugin");
-  await setTimeout(10 * 1000);
+  // await setTimeout(10 * 1000);
   appendFile(
     resolve("./dist/index.html"),
-    `<div>Plugin run ${Date.now()}</div>`
+    `<div>Plugin run ${new Date()}</div>`
   );
   console.log("done plugin");
 };
