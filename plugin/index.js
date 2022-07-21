2838b106ded68231fb0834aaf9dc9e55d454d66b
JGAntunes <joao@netlify.com>
2022-07-21 20:24:49 +0100

    test broken control flow logic in plugin

--- plugin/index.js
+++ plugin/index.js
@@ -7,12 +7,12 @@ export const onPostBuild = async function () {
   console.log("start plugin");
   // await setTimeout(10 * 1000);
   for (let i = 0; i < 300; i++) {
-    await appendFile(
+    appendFile(
       resolve(`./dist/index-${i}.html`),
       `<div>Plugin run ${new Date()}</div>`
     );
   }
-  await appendFile(
+  appendFile(
     resolve(`./dist/index.html`),
     `<div>Plugin run ${new Date()}</div>`
   );
